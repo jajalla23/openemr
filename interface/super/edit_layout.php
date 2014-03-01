d8226c427b7ce36bf52ba3787d7fa44589a3ddc1
jajalla23 <jajalla23@gmail.com>
2014-03-01 17:45:58 -0500

    reverting back changes for cleaner check in

--- interface/super/edit_layout.php
+++ interface/super/edit_layout.php
@@ -55,7 +55,6 @@ $datatypes = array(
   "33" => xl("Race and Ethnicity"),
   "34" => xl("NationNotes"),
   "35" => xl("Facilities"),
-  "36" => xl("Multiselect List Box")
 );
 
 function nextGroupOrder($order) {
